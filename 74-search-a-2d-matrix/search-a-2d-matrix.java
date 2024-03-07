2fd5806772ae330a0492e3033fd7e896756cd5e0
AishwaryaJorve <67336425+AishwaryaJorve@users.noreply.github.com>
2024-03-07 18:29:43 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (52.51%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,7 +1,28 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        return bruteForce(matrix,target);
+        // return bruteForce(matrix,target);
+        return optimizeSolution(matrix,target);
     }
+
+    public boolean optimizeSolution(int[][] matrix, int target){
+        int low=0;
+        int high=(matrix.length*matrix[0].length)-1;
+
+        while(low<=high){
+           int mid=(low+high)/2;
+
+           int row=mid/matrix[0].length;
+           int col=mid%matrix[0].length;
+
+           if(matrix[row][col]==target) return true;
+           else if(matrix[row][col]<target) 
+                low=mid+1;
+           else 
+                high=mid-1;
+        }
+
+        return false;
+    }    
 
     public boolean bruteForce(int[][] matrix, int target){
            for(int row=0;row<matrix.length;row++){
