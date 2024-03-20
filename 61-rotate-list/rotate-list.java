df5606d52abc2ecbc382d28b84756ec0c1c9df9f
AishwaryaJorve <67336425+AishwaryaJorve@users.noreply.github.com>
2024-03-20 11:47:02 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (75.84%) - LeetSync

--- 61-rotate-list/rotate-list.java
+++ 61-rotate-list/rotate-list.java
@@ -11,20 +11,28 @@
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
 
-        if(head==null)
-           return null;
-        
         int length=1;
         ListNode tail=head;
 
+        if(head==null || k==0)
+           return head;
+
+        // Find the length and tail element of linkedList
         while(tail.next!=null){
             length++;
             tail=tail.next;
         }
 
+        // If Suppose LinkedList length is 5 and k is coming 5, 10,15 (Fully Divisible by 5 i.e k) so after rotate it will come at head so if K is fully divisible by length should return head
         if(k%length==0) return head;
-        int newK=k%length;
+
+        // Linked tail node with head (Now our LinkedList is circular linkedlist)
         tail.next=head;
+        
+        //If k not fully Divisible by length find out new K e.g = k=14 and length is 5 newK = 4(Reminder)
+        int newK=k%length;
+
+        //Pass head and length-newk so 5-4=1
         ListNode newTail= findNewTail(head,length-newK);
 
         head=newTail.next;
