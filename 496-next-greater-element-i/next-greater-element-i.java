05d9fb92a064e67d3a776319d0817c5c80955fb1
AishwaryaJorve <67336425+AishwaryaJorve@users.noreply.github.com>
2024-04-13 18:32:05 +0530

    Time: 3 ms (90.20%) | Memory: 43.4 MB (64.05%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -4,8 +4,11 @@ class Solution {
       Map<Integer,Integer> nextGretest=new HashMap();
       Stack<Integer> stack=new Stack();
 
+    // Explination example nums2=[6,5,4,3,2,1,7]
       for(int i=0;i<nums2.length;i++){
-        while(!stack.isEmpty() && stack.peek()<nums2[i]){
+        // Used while because stack is [1,2,3,4,5,6] and now num2[i] is 7 so it should pop every element from stack and for all 
+        // next greaterElement is 7 
+        while(!stack.isEmpty() && stack.peek()<nums2[i]){    
             nextGretest.put(stack.pop(),nums2[i]);
         }
 
