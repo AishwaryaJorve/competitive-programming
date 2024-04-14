061d5ad3d651c5f05fcbb8b16cd0b1875ddabfd2
AishwaryaJorve <67336425+AishwaryaJorve@users.noreply.github.com>
2024-04-14 17:04:45 +0530

    Time: 9 ms (89.82%) | Memory: 46.6 MB (12.00%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -5,9 +5,11 @@ class Solution {
         Stack<Integer> stack=new Stack();
         int[] resArray=new int[nums.length];
         Arrays.fill(resArray, -1);
-        for(int i=0;i<length;i++){
-            int num=nums[(i%nums.length)];
-            
+        //Example [1,2,3,4,3]
+        for(int i=0;i<length-1;i++){
+            int num=nums[(i%nums.length)];   
+            // In above line  0%5= 5 zeroja zero so remider 0 / 1%5 5 zeroja zero reminder (1-0=1) / 2%5 5 zeroja zero reminder (2-0=2)....
+            // 5%5 = 0 / 6%5 = 5 oneja 5 (6-5=1) / 7%5 = 5 oneja 5 (7-5=2) .... This is how we can iterate array twise 
             while(!stack.isEmpty() && nums[stack.peek()]<num){
                  resArray[stack.pop()]=num;
             }
